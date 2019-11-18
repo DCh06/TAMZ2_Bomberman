ecb84108b587d9b035dad90e51be91c0c8d2e892
Daniel Choleva <dch@choleva.cz>
2019-11-18 20:36:50 +0100

    gamepanel

--- app/src/main/java/com/example/bombermangitversion/MainActivity.java
+++ app/src/main/java/com/example/bombermangitversion/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.bombermangitversion;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new GamePanel(this));
     }
 }
